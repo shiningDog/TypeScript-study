e4ac7c33ec573de3712cfb3aedfe29909efadc30
unknown <W9010685@adc.com>
2022-03-29 11:49:32 +0800

    update code

--- 01-ts-basic/hello.ts
+++ 01-ts-basic/hello.ts
@@ -1,7 +1,12 @@
 // 你好 世界
 // console.log("hello world");
 
-function greet(person, date) {
-  console.log(`helllo ${person} ,today is ${date}`);
+function greet(person: String, date: Date) {
+  console.log(`helllo ${person} ,today is ${date}.`);
 }
-greet("魏泽", "2022-03-28");
+greet("魏泽", new Date());
+
+// let message = "hello there!";
+// message = "hello world!";
+// message = 100;//error : 不能将类型“number”分配给类型“string”
+
