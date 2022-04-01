10eec02d4a19a26d2c126efe8c989e24fe4688d6
unknown <W9010685@adc.com>
2022-04-01 19:12:56 +0800

    update code

--- 02-everyday-types/dist/10-type-assertions.js
+++ 02-everyday-types/dist/10-type-assertions.js
@@ -1,12 +1,12 @@
 "use strict";
-// 断言 建议使用 as
-const myCanvas = document.getElementById("main_canvas");
-const myCanvas2 = document.getElementById("main_canvas");
-// let x = (<any>"hello") as number;
-let x = "hello";
-let y = "hello";
-// 总结：
-// 断言是为了保证未知数据格式的一种校验方式
-// 使用场景：
-// 1.当你确定未知数据是某一种格式时
-// 2.当你大概知道未知数据格式时
+// // 断言 建议使用 as
+// const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
+// const myCanvas2 = <HTMLCanvasElement>document.getElementById("main_canvas");
+// // let x = (<any>"hello") as number;
+// let x = "hello" as any as number;
+// let y = (<unknown>"hello") as number;
+// // 总结：
+// // 断言是为了保证未知数据格式的一种校验方式
+// // 使用场景：
+// // 1.当你确定未知数据是某一种格式时
+// // 2.当你大概知道未知数据格式时
---
+++ 02-everyday-types/dist/11-literal-types.js
@@ -0,0 +1,51 @@
+"use strict";
+/** 文字类型 */
+let testString = "hello world";
+testString = "魏泽";
+const constantString = "hello world";
+// constantString = "魏泽"; //error 无法分配，因为它是常数
+let x = "hello";
+// x = "world";// error 不能讲类型'world' 分配给 ‘hello’
+function printText(s, alignment) { }
+// printText("hello", "test");//error 第二个参数由于定义了定制的联合类型
+printText("hello", "left");
+function compare(a, b) {
+    return a === b ? 0 : a > b ? 1 : -1;
+}
+compare(0, 0);
+compare(0, 1);
+compare(1, 0);
+function configure(x) { }
+configure({
+    width: 100,
+});
+configure("auto");
+// configure('dasdsa')//error 参数定义了定制的联合类型
+let b1 = true;
+let b2 = false;
+let b3 = true;
+b3 = false;
+const obj = {
+    count: 0,
+};
+obj.count++;
+function hanlderRequest(url, method) { }
+// const req = {
+//   url: "https://example.com",
+//   method: "GET",
+// } as const;
+// hanlderRequest(req.url, req.method);
+// const req = {
+//   url: "https://example.com",
+//   method: "GET" as 'GET'
+// hanlderRequest(req.url, req.method);
+// const req = {
+//   url: "https://example.com",
+//   method: "GET",
+// };
+// hanlderRequest(req.url, req.method as "GET");
+// const req = {
+//   url: "https://example.com",
+//   method: "GET",
+// };
+// hanlderRequest(req.url, req.method); //error req.method 是string类型，不是指定的'GET' | 'POST' | 'GUSS'
